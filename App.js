b5e9b4488d944a4888d3c9342cb7db05b3b9d77b
Paul Bond <paulandrewbond@outlook.com>
2020-02-17 18:44:31 +0000

    add location request

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default class App extends Component {
   state = {
-    location: null
+    location: null,
+    errorMessage: null
   };
+
+  componentDidMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        errorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!"
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
   render() {
+    let text = "Waiting..";
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.paragraph}>{text}</Text>
       </View>
     );
   }
